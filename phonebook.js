23e8f539225761cbf29b806d881f32101e1aa85e
Henri Seppa <henri.seppa@tuni.fi>
2023-07-22 15:45:16 +0300

    implemented exercise 3.5

--- phonebook.js
+++ phonebook.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {
       id: 1,
@@ -40,7 +42,35 @@ app.get('/api/persons/:id', (req,res) => {
     } else {
       res.status(404).end()
     }
-  })
+})
+
+const generateId = () => {
+    return Math.floor(Math.random() * 10000)
+}
+
+app.post('/api/persons', (req,res) => {
+    const body = req.body
+
+    if (!body.name) {
+        return res.status(400).json({ 
+            error: 'Name is missing' 
+        })
+    }
+    else if (!body.number) {
+        return res.status(400).json({ 
+            error: 'Number is missing' 
+        })
+    }
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person)
+    res.json(person)
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
